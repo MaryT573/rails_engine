5144149fa2d84ad13354ee1bf10600da1f9dcaf7
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-08-31 15:46:52 -0600

    create test for displaying single merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -21,8 +21,20 @@ describe "merchant API" do
     end
   end
 
-  xit 'finds one merchant' do
-    
+  it 'finds one merchant' do
+    merchant = create(:merchant)
+    #require 'pry'; binding.pry 
+    get "/api/v1/merchants/#{merchant.id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(String)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a(String)
   end
 
   xit 'it returns all items for single merchant' do
