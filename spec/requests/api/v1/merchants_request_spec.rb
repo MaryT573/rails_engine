6a266c7d04d075c3098896f51fb63f688d8e1dc7
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-08-31 00:03:33 -0600

    tests and relationships for items and merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 describe "merchant API" do
-  it "" do
+  it "finds all merchants" do
 
+  end
+
+  xit 'finds one merchant' do
+    
+  end
+
+  xit 'it returns all items for single merchant' do
+    
   end
 end
