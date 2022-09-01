71154e9ff5ff49dfacd257c8d116f664eae7ad11
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-08-31 23:19:48 -0600

    add test for show single item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -33,4 +33,30 @@ describe "item API" do
             expect(item[:attributes][:merchant_id]).to be_a(Integer)
           end
     end
+
+    it 'can return a single item' do
+        merchant = create(:merchant)
+        item = create_list(:item, 1, merchant_id: merchant.id)
+        
+        get "/api/v1/items/#{item.id}"
+
+        expect(response).to be_successful
+
+        item = JSON.parse(response.body, symbolize_names: true)
+
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_an(String)
+      
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+      
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+      
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+      
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
 end
