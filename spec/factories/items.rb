a0a27cc1966514fcdb33bce48b8c8c1302aa2a6e
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-08-31 22:58:55 -0600

    returns items of a merchant

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Coffee.blend_name }
+    description { Faker::TvShows::Simpsons.quote }
+    unit_price { Faker::Number.within(range: 0.0..1.0) }
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -37,7 +37,33 @@ describe "merchant API" do
     expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 
-  xit 'it returns all items for single merchant' do
+  it 'it returns all items for single merchant' do
+    merchant = create(:merchant)
+    merchant.items = create_list(:item, 3, merchant_id: merchant.id)
     
+    get "/api/v1/merchants/#{merchant.id}/items"
+    
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant[:data].count).to eq(3)
+
+    merchant[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
   end
 end
